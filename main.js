4ea23cf317695b95ef103c82693b7114eb9ec468
LDQ-first <178848890@qq.com>
2019-07-29 16:55:42 +0800

    'modify'

--- main.js
+++ main.js
@@ -22,11 +22,12 @@ var print = function () {
     // 'height = 300, width = 700, top = 100, left = 100, toolbar=no, menubar=no, scrollbars=no,resizable=no,location=no,status=no')
     var wind = window.open('', 'newwindow', '')
     var printCss = './print.css'
-    wind.document.write('<html><head><title>Print it!</title><link rel="stylesheet" type="text/css" href="print.css"><script src="./main.js"></script></head><body></body></html>')
+    wind.document.write('<html><head><title>Print it!</title><link rel="stylesheet" type="text/css" href="print.css"></script></head><body></body></html>')
     // wind.document.head.innerHTML = '<link rel="stylesheet" type="text/css" href="' + printCss + '">'
     wind.document.body.innerHTML = printContent
     wind.print()
     // wind.close()
 }
 
+console.log('btn', btn)
 btn.addEventListener('click', print)
