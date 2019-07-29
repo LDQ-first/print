be57d8b49eb1275847c899328edb082fc92ad950
LDQ-first <178848890@qq.com>
2019-07-29 23:15:07 +0800

    basically completed

---
+++ IEprint.js
@@ -0,0 +1,44 @@
+console.log('IEprint.js')
+function getExplorer() {
+  var explorer = window.navigator.userAgent
+  //ie 
+  if (explorer.indexOf('MSIE') >= 0) {
+    return 'IE'
+  }
+  //firefox 
+  else if (explorer.indexOf('Firefox') >= 0) {
+    return 'Firefox'
+  }
+  //Chrome
+  else if (explorer.indexOf('Chrome') >= 0) {
+    return 'Chrome'
+  }
+  //Opera
+  else if (explorer.indexOf('Opera') >= 0) {
+    return 'Opera'
+  }
+  //Safari
+  else if (explorer.indexOf('Safari') >= 0) {
+    return 'Safari'
+  }
+}
+
+if (getExplorer() == 'IE') {
+  pagesetup_null()
+}
+
+function pagesetup_null() {
+  var hkey_root, hkey_path, hkey_key
+  hkey_root = 'HKEY_CURRENT_USER'
+  hkey_path = '\\Software\\Microsoft\\Internet Explorer\\PageSetup\\'
+  try {
+    var RegWsh = new ActiveXObject('WScript.Shell')
+    hkey_key = 'header'
+    RegWsh.RegWrite(hkey_root + hkey_path + hkey_key, '')
+    hkey_key = 'footer'
+    RegWsh.RegWrite(hkey_root + hkey_path + hkey_key, '')
+  } catch (e) {
+    alert('错误：', e)
+  }
+}
+
--- main.js
+++ main.js
@@ -3,31 +3,31 @@ var btn = document.getElementsByClassName('btn')[0]
 
 
 
-var print = function () {
+var printFun = function () {
     var print = document.getElementsByClassName('print')[0]
     var printHtml = print.innerHTML
 
-   // window.print()
 
     // 直接调用 window.print()
-    /* var oldstr = document.body.innerHTML
+    var oldstr = document.body.innerHTML
     document.body.innerHTML = printHtml
     window.print()
-    document.body.innerHTML = oldstr */
+
+    document.body.innerHTML = oldstr
+    var btn = document.getElementsByClassName('btn')[0]
+    btn.addEventListener('click', printFun)
+
 
     // 使用window.open() + window.print()
-
-    var printContent = document.getElementsByClassName('print')[0].innerHTML
+    /* var printContent = document.getElementsByClassName('print')[0].innerHTML
     // var wind = window.open('', 'newwindow', 
     // 'height = 300, width = 700, top = 100, left = 100, toolbar=no, menubar=no, scrollbars=no,resizable=no,location=no,status=no')
     var wind = window.open('', 'newwindow', '')
-    var printCss = './print.css'
-    wind.document.write('<html><head><title>Print it!</title><link rel="stylesheet" type="text/css" href="print.css"></script></head><body></body></html>')
-    // wind.document.head.innerHTML = '<link rel="stylesheet" type="text/css" href="' + printCss + '">'
+    wind.document.write('<!DOCTYPE html><html><head><title>Print it!</title><link rel="stylesheet" type="text/css" href="print.css"></script></head><body></body></html>')
     wind.document.body.innerHTML = printContent
     wind.print()
-    // wind.close()
+    // wind.close() */
 }
 
-console.log('btn', btn)
-btn.addEventListener('click', print)
+
+btn.addEventListener('click', printFun)
